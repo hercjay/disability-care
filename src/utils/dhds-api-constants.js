864044c0034a4ffc81ce6c8ec09bf6194adcfdb0
hercjay <iamhercjay@gmail.com>
2024-05-21 19:31:02 +0100

    add relevant keys for DHDS CDC API filters

---
+++ src/utils/dhds-api-constants.js
@@ -0,0 +1,48 @@
+const dhdsConstants = {
+    stratification1: [
+      { key: "SELFDIS", title: "Self-care Disability" },
+      { key: "INDDIS", title: "Independent Living Disability" },
+      { key: "COGDIS", title: "Cognitive Disability" },
+      { key: "HEARDIS", title: "Hearing Disability" },
+      { key: "VISDIS", title: "Vision Disability" },
+      { key: "MOBDIS", title: "Mobility Disability" },
+    //   { key: "NODIS", title: "No Disability" },
+      { key: "DISABL", title: "Any Disability" }
+    ],
+    stratificationcategory1: [
+      { key: "DISCTYPE", title: "Disability Type" },
+      { key: "DISSTAT", title: "Disability Status" }
+    ],
+    categoryid: [
+      { key: "DEMOG", title: "Demographics" },
+      { key: "DISEST", title: "Disability Estimates" },
+      { key: "HLTHRB", title: "Health Risks & Behaviors" }
+    ],
+    indicatorid: [
+      { key: "JOB", title: "Employment status" },
+      { key: "EDUCATE", title: "Education level" },
+      { key: "INCOMEN", title: "Income level" },
+      { key: "VETIND", title: "Veteran status" },
+      { key: "MARRIED", title: "Marital status" },
+      { key: "BMI", title: "Body mass index category" },
+      { key: "SMOKE", title: "Smoking status" },
+      { key: "BINGE", title: "Binge drinking in the past 30 days" }
+    ],
+    responseid: [
+      { key: "JOB01", title: "Employed" },
+      { key: "JOB04", title: "Other" },
+      { key: "EDUCATE03", title: "College Graduate" },
+      { key: "INCOME03", title: "$25,000 to <$35,000" },
+      { key: "VET1", title: "Veteran" },
+      { key: "MARRIED03", title: "Widowed" },
+      { key: "BMI01", title: "Underweight" },
+      { key: "SMOKE03", title: "Never Smoker" },
+      { key: "BINGE02", title: "No" }
+    ],
+    datavaluetypeid: [
+      { key: "AGEADJPREV", title: "Age-adjusted Prevalence" },
+      { key: "PREV", title: "Prevalence" }
+    ]
+  };
+  
+  export default dhdsConstants;
