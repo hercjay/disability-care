b19fc64b1c6a63b24420a85f7d6f7f6d74bfa5fc
hercjay <iamhercjay@gmail.com>
2024-05-22 18:28:16 +0100

    apply scaler and increase max iteration to 1000 to improve model accuracy

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 # Load the filtered dataset
-df = pd.read_csv('dhds-cancer.csv')
+df = pd.read_csv('dhds-cancer.csv', low_memory=False)
 
 # Split the dataset into training and testing sets
 X = df.drop('Response', axis=1)  # features
@@ -19,11 +19,15 @@ le = LabelEncoder()
 X = X.apply(le.fit_transform)
 y = le.fit_transform(y)
 
+# scale the features to improve accuracy of the model
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
 # split into training sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train a logistic regression model on the training data
-model = LogisticRegression(random_state=42)
+model = LogisticRegression(random_state=42, max_iter=1000)
 model.fit(X_train, y_train)
 
 # Evaluate the model's performance on the testing data
