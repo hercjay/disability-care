fb42f6700bdc4b80aa06a10c5c2f218bbc137162
hercjay <iamhercjay@gmail.com>
2024-05-22 11:56:34 +0100

    work with a larger dataset (has more columns) and use labelEncoder to preprocess all columns

--- backend/app.py
+++ backend/app.py
@@ -2,18 +2,22 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
+from sklearn.preprocessing import LabelEncoder
 
 # Load the filtered dataset
-df = pd.read_csv('dhds-cancer-age.csv')
+df = pd.read_csv('dhds-cancer.csv')
 
 # Split the dataset into training and testing sets
 X = df.drop('Response', axis=1)  # features
 y = df['Response']  # target variable
 
-# Age range (18-44, 45-64, 65+), map this string values to integers
-X['Stratification2'] = X['Stratification2'].map({'18-44': 0, '45-64': 1, '65+': 2})
-X['Stratification1'] = X['Stratification1'].map({'No Disability': 0, 'Any Disability': 1})
-y['Response'] = y['Response'].map({'Yes': 1, 'No': 0})
+# Convert all columns to strings because encoder can only work with one type per column
+X = X.astype(str)
+
+# encode all labels using label encoder
+le = LabelEncoder()
+X = X.apply(le.fit_transform)
+y = le.fit_transform(y)
 
 # split into training sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
